1b753e4f539199b1733b8afd02516a2a79e8aa01
Troniclabs <troniclabs.crew@gmail.com>
2023-07-13 12:18:25 +0200

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "TronicLabs/scoop_bucket_erb_win:main"
+    [String]$upstream = "TronicLabs/troniclabs-buckets:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
